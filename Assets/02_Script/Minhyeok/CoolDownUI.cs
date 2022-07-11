cc774f43071df9b8a02b86c9ca75f381a8b77c3f
minhyeok8278 <98887903+minhyeok8278@users.noreply.github.com>
2022-07-12 00:51:24 +0900

    스킬 아이콘 쿨타임

---
+++ Assets/02_Script/Minhyeok/CoolDownUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolDownUI : MonoBehaviour
+{
+    [SerializeField] Image fill;
+    [SerializeField] float maxCoolDown;
+    [SerializeField] float currentCoolDown;
+    bool UseSkill = false;
+
+    private void Awake()
+    {
+        currentCoolDown = maxCoolDown;
+    }
+
+    public void SetMaxCoolDown(float value)
+    {
+        maxCoolDown = value;
+        
+    }
+    public void SetCurrentCoolDown(float value)
+    {
+        currentCoolDown = value;
+
+    }
+    public void UpdateFillAmount()
+    {
+        fill.fillAmount = Mathf.Lerp(0,1,currentCoolDown/maxCoolDown);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            UseSkill = true;
+        if(UseSkill == true)
+        {
+            currentCoolDown -= Time.deltaTime;
+            UpdateFillAmount();
+            if (currentCoolDown <= 0f)
+            {
+                currentCoolDown = maxCoolDown;
+                UseSkill = false;
+            }
+        }
+    }
+}
