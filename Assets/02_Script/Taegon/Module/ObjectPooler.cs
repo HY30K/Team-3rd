973f90f8c178ee7d51b3b96f8d8703331aa6dfd8
Taegon Lee <ltg9amer@naver.com>
2022-07-11 23:53:15 +0900

    오브젝트 풀링

---
+++ Assets/02_Script/Taegon/Module/ObjectPooler.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private int poolSize;
+    private Dictionary<string, Stack<GameObject>> poolers = new Dictionary<string, Stack<GameObject>>();
+
+    public Dictionary<string, Stack<GameObject>> Poolers => poolers;
+
+    private void Awake()
+    {
+        for (int i = 0; i < prefabs.Length; i++)
+        {
+            for (int j = 0; j < poolSize; j++)
+            {
+                if (!poolers.ContainsKey(prefabs[i].name))
+                {
+                    poolers.Add(prefabs[i].name, new Stack<GameObject>());
+                }
+
+                GameObject prefab = Instantiate(prefabs[i]);
+                prefab.name = prefab.name.Replace("(Clone)", null);
+
+                prefab.SetActive(false);
+                poolers[prefabs[i].name].Push(prefab);
+            }
+        }
+    }
+
+    public GameObject SpawnPrefab(string key)
+    {
+        GameObject prefab = null;
+
+        if (poolers[key].Count > 0)
+        {
+            prefab = poolers[key].Pop();
+            prefab.SetActive(true);
+        }
+        else
+        {
+            prefab = Instantiate(Resources.Load($"03_Prefab/{key}") as GameObject);
+            prefab.name = prefab.name.Replace("(Clone)", null);
+        }
+
+        return prefab;
+    }
+
+    public void DespawnPrefab(GameObject prefab)
+    {
+        prefab.SetActive(false);
+        poolers[prefab.name].Push(prefab);
+    }
+}
