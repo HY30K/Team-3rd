d9b672f304f50644bc514b293532bb4aa1d5bb06
minhyeok8278 <98887903+minhyeok8278@users.noreply.github.com>
2022-07-12 14:26:57 +0900

    dotween pro 임포트,튜토리얼 작업 시작

---
+++ Assets/02_Script/Minhyeok/AppearText.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+public class AppearText : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
