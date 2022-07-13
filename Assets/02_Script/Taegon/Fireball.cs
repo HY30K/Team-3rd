117f3e1b6f850b36ed7014c323fbff3e61c3e84b
minhyeok8278 <98887903+minhyeok8278@users.noreply.github.com>
2022-07-13 15:17:23 +0900

    스크립트 수정

--- Assets/02_Script/Taegon/Fireball.cs
+++ Assets/02_Script/Taegon/Fireball.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Fireball : MonoBehaviour
 {
     [SerializeField] private LayerMask enemyLayer;
-    [SerializeField] private Vector2 hitPosition;
+    [SerializeField] private Transform hitPosition;
     [SerializeField] private Player player;
     [SerializeField] private float hitSize;
 
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
     private void Update()
     {
-        foreach (Collider2D enemy in Physics2D.OverlapCircleAll(hitPosition, hitSize, enemyLayer))
+        foreach (Collider2D enemy in Physics2D.OverlapCircleAll(hitPosition.position, hitSize, enemyLayer))
         {
             enemy.GetComponent<Enemy>().OnDamage(player.ATKLevel + player.ATKSkillLevel);
 
