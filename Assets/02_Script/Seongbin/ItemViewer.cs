33b21c347dbfcc386cdbe9c0a1ddfa0af17b789a
인희성빈 <seongbin529@gmail.com>
2022-07-13 10:43:07 +0900

    상점업그뤠이드

--- Assets/02_Script/Seongbin/ItemViewer.cs
+++ Assets/02_Script/Seongbin/ItemViewer.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
-public class Item : MonoBehaviour
+public class ItemViewer : MonoBehaviour
 {
     public enum ItemType
     {
@@ -13,45 +12,21 @@ public class Item : MonoBehaviour
         Water
     }
     public ItemType itemType;
-    Rigidbody2D rig;
     public SpriteRenderer spriteRenderer;
-    public float followSpeed;
-    public float power;
-
     [SerializeField] private List<Sprite> itemIamge = new List<Sprite>();
 
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Update is called once per frame
+    void Update()
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            GetItem();
-            Destroy(gameObject, 0.75f);
-        }
+        
     }
-    private void GetItem()//statup
-    {
-        switch (itemType)
-        {
-            case ItemType.Protein:
-                break;
-            case ItemType.Chicken:
-                break;
-            case ItemType.Energybar:
-                break;
-            case ItemType.Water:
-                break;
-            //stat up
-            default:
-                break;
-        }
-    }
-    public void ItemSet()
+    public void ItemViewers()
     {
         switch (itemType)
         {
@@ -70,11 +45,5 @@ public class Item : MonoBehaviour
             default:
                 break;
         }
-    }
-    public void SpawnItem()
-    {
-        gameObject.SetActive(true);
-        rig.AddForce(new Vector2(0, 10) * power);
-        Invoke("canGetItem", 1f);
     }
 }
