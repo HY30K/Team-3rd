05c93c6952e50250921b6c1875d812b68b1a61bf
인희성빈 <seongbin529@gmail.com>
2022-07-13 19:49:55 +0900

    아 야발 메인작업 ~~

--- Assets/02_Script/Seongbin/StageChange.cs
+++ Assets/02_Script/Seongbin/StageChange.cs
@@ -9,7 +9,6 @@ public class StageChange : MonoBehaviour
     [SerializeField] private Transform stage2Target; // 이동할 타겟 설정
     [SerializeField] private Transform stage3Target; // 이동할 타겟 설정
     [SerializeField] private Transform shopTarget; // 이동할 타겟 설정
-    [SerializeField] private Transform bossStageTarget; // 이동할 타겟 설정
 
     public enum ChangeStage
     {
@@ -17,11 +16,11 @@ public class StageChange : MonoBehaviour
         stage2,
         stage3,
         shop,
-        boss
     }
     // Start is called before the first frame update
     public void Awake()
     {
+
     }
 
     // 박스 콜라이더에 닿는 순간 이벤트 발생
@@ -50,9 +49,6 @@ public class StageChange : MonoBehaviour
                 break;
             case ChangeStage.shop:
                 GameObject.Find("Player").transform.position = shopTarget.transform.position;
-                break;
-            case ChangeStage.boss:
-                GameObject.Find("Player").transform.position = bossStageTarget.transform.position;
                 break;
             default:
                 break;
